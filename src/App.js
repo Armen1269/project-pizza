ca043379f3c71051b1dc609000cae859a6a2da04
Armen1269 <armen23arm@gmail.com>
2023-05-17 13:23:35 +0400

    Das 5 sax normal

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./scss/app.scss";
+import Header from "./Components/Header/Header";
+import Catigories from "./Components/Content-top/Catigories/Catigories";
+import Sort from "./Components/Content-top/Sort/Sort";
+import pizzas from "./assets/pizzas.json";
+import PizaaBlock from "./Components/PizzaBlock/PizzaBlock";
+console.log(pizzas);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <Header />
+      <div className="content">
+        <div className="container">
+          <div className="content__top">
+            <Catigories />
+            <Sort />
+          </div>
+          <h2 className="content__title">Все пиццы</h2>
+          <div className="content__items">
+            {pizzas.map((obj) => (
+              <PizaaBlock key={obj.id}
+                {...obj} // karelia  nerqevi  sax graci  poxaren ogtvel SPRED operatoris  aysinqn {...obj}
+                // key={obj.title}
+                // title={obj.title}
+                // price={obj.price}
+                //imageUrl={obj.imageUrl}
+                // sizes={obj.sizes}
+                // types ={obj.types}
+              />
+            ))}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/Components/Content-top/Catigories/Catigories.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+const Catigories = () => {
+  const [activeCatigories, setActiveCatigories] = useState(0);
+
+  const onClicCatigories = (index) => {
+    setActiveCatigories(index);
+  };
+
+  const catigoriesPizza = [
+    "Все",
+    "Мясные",
+    "Вегетарианская",
+    "Гриль",
+    "Острые",
+    "Закрытые",
+  ];
+  return (
+    <div className="categories">
+      <ul>
+        {/* <li className="active">Все</li> */}
+        {catigoriesPizza.map((value,index) => {
+          return (
+            <li  key={index}
+              onClick={() => onClicCatigories(index)}
+              className={activeCatigories === index ? "active" : ""}
+            >
+              {value}{" "}
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
+export default Catigories;
