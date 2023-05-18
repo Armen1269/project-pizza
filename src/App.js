73f7a108254134e892aa366d996d6d5a2d250336
Armen1269 <armen23arm@gmail.com>
2023-05-18 11:32:43 +0400

    JSON faili poxaren  ogtvum enq FETCHIC

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./scss/app.scss";
 import Header from "./Components/Header/Header";
 import Catigories from "./Components/Content-top/Catigories/Catigories";
 import Sort from "./Components/Content-top/Sort/Sort";
-import pizzas from "./assets/pizzas.json";
+//import pizzas from "./assets/pizzas.json";  -//  JSON  failic  anjatvum  enq  vorpezi  nuyn  fail@   stananq  serveric
 import PizaaBlock from "./Components/PizzaBlock/PizzaBlock";
-console.log(pizzas);
+//console.log(pizzas);
 
 function App() {
+  const [items, setItems] = useState([]);
+  useEffect(() => {
+    fetch(`https://6465c337228bd07b3551cd41.mockapi.io/items`)
+      .then((response) => response.json())
+      .then((items) => setItems(items));
+  },[]);
+console.log(items);
   return (
     <div className="wrapper">
       <Header />
@@ -19,17 +26,24 @@ function App() {
           </div>
           <h2 className="content__title">Все пиццы</h2>
           <div className="content__items">
-            {pizzas.map((obj) => (
-              <PizaaBlock key={obj.id}
-                {...obj} // karelia  nerqevi  sax graci  poxaren ogtvel SPRED operatoris  aysinqn {...obj}
-                // key={obj.title}
-                // title={obj.title}
-                // price={obj.price}
-                //imageUrl={obj.imageUrl}
-                // sizes={obj.sizes}
-                // types ={obj.types}
-              />
+            {/* ------------------FETCH - i   mijocov ------------------------------ */}
+
+            {items.map((obj) => (
+              <PizaaBlock key={obj.id} {...obj} /> 
             ))}
+            {/* ---------------------------------------------------------------------- */}
+            {/* ---  ajs  varyan@  <<JSON>> -  i mijocov e ----- isk  sranic verev@  <<FETCHI>>   mijocov------- */}
+            {/* {pizzas.map((obj) => (
+             <PizaaBlock
+               key={obj.id}
+                {...obj} karelia  nerqevi  sax graci  poxaren ogtvel SPRED operatoris  aysinqn {...obj}
+                //title={obj.title}
+                //price={obj.price}
+              //imageUrl={obj.imageUrl}
+                //sizes={obj.sizes}
+                //types ={obj.types}
+              />
+            ))} */}
           </div>
         </div>
       </div>
