699022a8d2db6b08e9aae3f8b4bd72c3a195ff75
Armen1269 <armen23arm@gmail.com>
2023-05-18 18:19:16 +0400

    katarvela <SKELETON> ajisnq minchev  pizzan fetchic  ga   maketner en  texet@ patkervum ev hajord qayl@ da mer routerne. nerka  es failic  heto stexcvum e  tarber  ejer  App ic   pizzae blokner@  texapoxvum en  urish arandzin ej  aysinqs  verenacvum  componentneri

--- src/App.js
+++ src/App.js
@@ -1,50 +1,44 @@
-import React, { useEffect, useState } from "react";
+// import React, { useEffect, useState } from "react";
 import "./scss/app.scss";
 import Header from "./Components/Header/Header";
-import Catigories from "./Components/Content-top/Catigories/Catigories";
-import Sort from "./Components/Content-top/Sort/Sort";
-//import pizzas from "./assets/pizzas.json";  -//  JSON  failic  anjatvum  enq  vorpezi  nuyn  fail@   stananq  serveric
-import PizaaBlock from "./Components/PizzaBlock/PizzaBlock";
-//console.log(pizzas);
+// import Catigories from "./Components/Content-top/Catigories/Catigories";
+// import Sort from "./Components/Content-top/Sort/Sort";
+// import PizzaBlockSkeleton from "./Components/PizzaBlock/PizzaBlockSkeleton";
+// import PizaaBlock from "./Components/PizzaBlock/PizzaBlock";
 
 function App() {
-  const [items, setItems] = useState([]);
-  useEffect(() => {
-    fetch(`https://6465c337228bd07b3551cd41.mockapi.io/items`)
-      .then((response) => response.json())
-      .then((items) => setItems(items));
-  },[]);
-console.log(items);
+  // const [pizzaBlockSkeleton, setPizzaplockSkeleton] = useState(true);
+  // const [items, setItems] = useState([]);
+  // const Arr = new Array(10).fill(undefined);
+  // console.log(Arr);
+  // console.log(items);
+  // useEffect(() => {
+  //   fetch(`https://6465c337228bd07b3551cd41.mockapi.io/items`)
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       setTimeout(() => {
+  //         setItems(data);
+  //         setPizzaplockSkeleton(false);
+  //       }, 1000);
+  //     });
+  // }, []);
+  // console.log(items);
   return (
     <div className="wrapper">
       <Header />
       <div className="content">
         <div className="container">
-          <div className="content__top">
+					
+          {/* <div className="content__top">
             <Catigories />
             <Sort />
           </div>
           <h2 className="content__title">Все пиццы</h2>
           <div className="content__items">
-            {/* ------------------FETCH - i   mijocov ------------------------------ */}
-
-            {items.map((obj) => (
-              <PizaaBlock key={obj.id} {...obj} /> 
-            ))}
-            {/* ---------------------------------------------------------------------- */}
-            {/* ---  ajs  varyan@  <<JSON>> -  i mijocov e ----- isk  sranic verev@  <<FETCHI>>   mijocov------- */}
-            {/* {pizzas.map((obj) => (
-             <PizaaBlock
-               key={obj.id}
-                {...obj} karelia  nerqevi  sax graci  poxaren ogtvel SPRED operatoris  aysinqn {...obj}
-                //title={obj.title}
-                //price={obj.price}
-              //imageUrl={obj.imageUrl}
-                //sizes={obj.sizes}
-                //types ={obj.types}
-              />
-            ))} */}
-          </div>
+            {pizzaBlockSkeleton
+              ? Arr.map((_, index) => <PizzaBlockSkeleton key={index} />)
+              : items.map((obj) => <PizaaBlock key={obj.id} {...obj} />)}
+          </div> */}
         </div>
       </div>
     </div>
@@ -52,3 +46,10 @@ console.log(items);
 }
 
 export default App;
+// {
+//               return pizzaBlockSkeleton ? (
+//                 <PizzaBlockSkeleton key={obj.id} {...obj} />
+//               ) : (
+//                 <PizaaBlock key={obj.id} {...obj} />
+//               );
+//             }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+
+import { useEffect, useState } from "react";
+import Catigories from "./Components/Content-top/Catigories/Catigories";
+import Sort from "./Components/Content-top/Sort/Sort";
+import PizzaBlockSkeleton from "./Components/PizzaBlock/PizzaBlockSkeleton";
+import PizaaBlock from "./Components/PizzaBlock/PizzaBlock";
+
+
+const Home = () => {
+	  const [pizzaBlockSkeleton, setPizzaplockSkeleton] = useState(true);
+    const [items, setItems] = useState([]);
+    const Arr = new Array(10).fill(undefined);
+    console.log(Arr);
+    console.log(items);
+    useEffect(() => {
+      fetch(`https://6465c337228bd07b3551cd41.mockapi.io/items`)
+        .then((response) => response.json())
+        .then((data) => {
+          setTimeout(() => {
+            setItems(data);
+            setPizzaplockSkeleton(false);
+          }, 1000);
+        });
+    }, []);
+    console.log(items);
+return (
+  <div>
+    <div className="content__top">
+      <Catigories />
+      <Sort />
+    </div>
+    <h2 className="content__title">Все пиццы</h2>
+    <div className="content__items">
+      {pizzaBlockSkeleton
+        ? Arr.map((_, index) => <PizzaBlockSkeleton key={index} />)
+        : items.map((obj) => <PizaaBlock key={obj.id} {...obj} />)}
+    </div>
+  </div>
+);
+
+
+
+}
+export default Home
